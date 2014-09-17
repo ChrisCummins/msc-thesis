f52cdefb73cfc273c3960978c6046d852272e0ef
Chris Cummins <chrisc.101@gmail.com>
2014-09-17 15:06:44 +0100

    exercises: Add a simple "Hello world!" MPI prog

---
+++ exercises/mpi/hello.cpp
@@ -0,0 +1,44 @@
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+#include "mpi.h"
+
+#define TIMESTAMP_BUFSIZE 64
+#define TIMESTAMP_FORMAT "%d %B %Y %I:%M:%S %p"
+
+void identify_process(int process_id) {
+    static char buf[TIMESTAMP_BUFSIZE];
+    const struct std::tm *tm;
+    std::time_t now;
+
+    now = std::time(NULL);
+    tm = std::localtime(&now);
+    std::strftime(buf, TIMESTAMP_BUFSIZE, TIMESTAMP_FORMAT, tm);
+
+    // Print timestamp and ID:
+    std::cout << "[" << buf << "] Process ID " << process_id << "\n";
+}
+
+
+int main(int argc, char *argv[]) {
+
+    int no_of_processes;
+    int process_id;
+    double wtime;
+
+    MPI::Init(argc, argv);
+
+    no_of_processes = MPI::COMM_WORLD.Get_size();
+    process_id = MPI::COMM_WORLD.Get_rank();
+
+    if (process_id == 0) {
+        std::cout << "Launching " << no_of_processes << " processes.\n";
+    }
+
+    identify_process(process_id);
+
+    MPI::Finalize();
+    return 0;
+};
