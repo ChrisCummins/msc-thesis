2c86c38df25510dfb7f050400c8ec4c6c96c7c82
Chris Cummins <chrisc.101@gmail.com>
2014-09-18 17:44:20 +0100

    exercises: Add simple C++ FIFO stack Template

---
+++ exercises/templates/stack.cpp
@@ -0,0 +1,27 @@
+#include "stack.h"
+
+// Test:
+int main(int argc, char *argv[]) {
+
+    Stack<int> s;
+
+    std::cout << s.toString() << "\n";
+
+    for (int i = 1; i <= 5; i++)
+        s.push(i);
+
+    std::cout << s.toString() << "\n";
+
+    std::cout << "               pop() = " << s.pop() << "\n";
+    std::cout << s.toString() << "\n";
+    
+    std::cout << "               peek() = " << s.peek() << "\n";
+    std::cout << s.toString() << "\n";
+
+    do {
+        std::cout << "               pop() = " << s.pop() << "\n";
+        std::cout << s.toString() << "\n";
+    } while (!s.empty());
+
+    return 0;
+}
---
+++ exercises/templates/stack.h
@@ -0,0 +1,109 @@
+#ifndef MSC_THESIS_EXERCISES_TEMPLATES_STACK_H_
+#define MSC_THESIS_EXERCISES_TEMPLATES_STACK_H_
+
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+
+/*
+ * A generic Stack type, implemented using a vector.
+ * 
+ * Oh yeah, and ridiculous caching of values, cos why not.
+ * Note: no actual performance gains under most circumstances.
+ */
+template<class T> 
+class Stack {
+
+ public:
+
+    Stack() {
+        this->cache.size_dirty = true;
+        this->cache.head_dirty = true;
+    }
+    
+    void push(const T elem) {
+        this->cache.size_dirty = true;
+        this->cache.head = elem;
+
+        stack.push_back(elem);
+    };
+
+    const T pop() {
+        T back = this->peek();
+
+        stack.pop_back();
+
+        this->cache.size_dirty = true;
+        this->cache.head_dirty = true;
+
+        return back;
+    };
+
+    const T peek() {
+        if (this->empty())
+            throw std::out_of_range("Stack<>::peek() Empty stack");
+
+        if (this->cache.head_dirty) {
+            this->cache.head = stack.back();
+            this->cache.head_dirty = false;
+        }
+
+        return this->cache.head;
+    };
+
+    const int length() {
+        if (this->cache.size_dirty) {
+            this->cache.size = stack.size();
+            this->cache.size_dirty = false;
+        }
+
+        return this->cache.size;
+    };
+
+    const bool empty() {
+        return stack.empty();
+    };
+
+    std::string toString() {
+        std::stringstream fmt;
+        fmt << "Stack[" << this << "]";
+        return fmt.str();
+    };
+
+ private:
+    std::vector<T> stack;
+
+    struct {
+        bool size_dirty;
+        int size;
+
+        bool head_dirty;
+        T head;
+    } cache;
+};
+
+
+/*
+ * Stack<int> specialization
+ */
+template <>
+std::string Stack<int>::toString() {
+    std::stringstream fmt;
+
+    fmt << this << " Stack<int>[" << this->length() << "] = { ";
+
+    for(std::vector<int>::size_type i = this->length() - 1; 
+        i != (std::vector<int>::size_type) - 1; i--) {
+        fmt << stack[i] << " ";
+    }
+
+    fmt << "}";
+
+    return fmt.str();
+}
+
+#endif // MSC_THESIS_EXERCISES_TEMPLATES_STACK_H_
