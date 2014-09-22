215b2ebfa757651ea626319ac8a87bd7cdb0b658
Chris Cummins <chrisc.101@gmail.com>
2014-09-23 00:38:04 +0100

    templates: Add a std library sort test

---
+++ exercises/templates/lib-sort.cc
@@ -0,0 +1,34 @@
+#include <algorithm>
+
+#include "list.h"
+
+void test_list_sort(void (*sort)(List::iterator, List::iterator), size_t size) {
+    List list = get_unsorted_list(size);
+
+    // Start of timed section
+    Timer timer;
+    sort(list.begin(), list.end());
+    unsigned int elapsed = timer.ms();
+    // End of timed section
+
+    assert(list_is_sorted(list));
+
+    printf("Time to sort %7lu integers: %4u ms\n", list.size(), elapsed);
+}
+
+void test_list_sort(void (*sort)(List::iterator, List::iterator),
+                    const char *name) {
+    int i;
+    size_t j;
+
+    std::printf("%s:\n", name);
+    for (i = 0, j = 200000; i < 10; i++, j += 200000)
+        test_list_sort(sort, j);
+}
+
+int main(int argc, char *argv[]) {
+    test_list_sort(std::sort, "std::sort");
+    test_list_sort(std::stable_sort, "std::sort_stable");
+
+    return 0;
+}
