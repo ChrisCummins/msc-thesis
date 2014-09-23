1c1ffe049669f350bf5721f0e600991eb7e5985a
Chris Cummins <chrisc.101@gmail.com>
2014-09-23 15:08:23 +0100

    templates/dac: Test with float vector

--- exercises/templates/dac.cc
+++ exercises/templates/dac.cc
@@ -1,21 +1,35 @@
 #include "dac.h"
 
-typedef DC<int>      MergeSort;
-typedef vector<int>  vector_t;
-
-void test_sort(const size_t size) {
-    int nums[size];
+void test_float_sort(const size_t size) {
+    float nums[size];
 
     for (size_t i = 0; i < size; i++)
-        nums[i] = rand() % size;
+        nums[i] = ((float)rand() / 100);
 
-    vector_t *const a = new vector_t;
+    vector<float> *const a = new vector<float>;
 
     a->data   = &nums[0];
     a->length = sizeof(nums) / sizeof(nums[0]);
 
     Timer t;
-    MergeSort sort(a);
+    DC<float> sort(a);
+    printf("Time to sort %7lu integers: %4ld ms\n", size, t.ms());
+    sort.get()->isSorted();
+}
+
+void test_int_sort(const size_t size) {
+    int nums[size];
+
+    for (size_t i = 0; i < size; i++)
+        nums[i] = rand() % size;
+
+    vector<int> *const a = new vector<int>;
+
+    a->data   = &nums[0];
+    a->length = sizeof(nums) / sizeof(nums[0]);
+
+    Timer t;
+    DC<int> sort(a);
     printf("Time to sort %7lu integers: %4ld ms\n", size, t.ms());
     sort.get()->isSorted();
 }
@@ -24,7 +38,11 @@ int main(int argc, char *argv[]) {
 
     std::cout << "DC<int>\n";
     for (unsigned long i = 0, j = 200000; i < 10; i++, j += 200000)
-        test_sort(j);
+        test_int_sort(j);
+
+    std::cout << "DC<float>\n";
+    for (unsigned long i = 0, j = 200000; i < 10; i++, j += 200000)
+        test_float_sort(j);
 
     return 0;
 }
