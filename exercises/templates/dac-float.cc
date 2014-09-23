6287beb12cbdf0f936d47e521dedd97b12adc9dc
Chris Cummins <chrisc.101@gmail.com>
2014-09-23 15:49:15 +0100

    templates/dac-(int|float): Set fork_depth in args

--- exercises/templates/dac-float.cc
+++ exercises/templates/dac-float.cc
@@ -1,6 +1,7 @@
 #include "dac.h"
 
-void test_float_sort(const size_t size) {
+void test_float_sort(const size_t size,
+                     const unsigned int fork_depth) {
     float nums[size];
 
     for (size_t i = 0; i < size; i++)
@@ -12,16 +13,25 @@ void test_float_sort(const size_t size) {
     a->length = sizeof(nums) / sizeof(nums[0]);
 
     Timer t;
-    DC<float> sort(a);
+    DC<float> sort(a, 2, fork_depth);
     printf("Time to sort %7lu integers: %4ld ms\n", size, t.ms());
     sort.get()->isSorted();
 }
 
 int main(int argc, char *argv[]) {
+    unsigned int fork_depth = 1;
 
-    std::cout << "DC<float>\n";
+    if (argc == 2) {
+        unsigned int i = atoi(argv[1]);
+        if (i)
+            fork_depth = i;
+        else
+            std::cout << "warning: <fork_depth> must be an integer. Ignoring argument\n";
+    }
+
+    std::cout << "DC<float>, fork_depth = " << fork_depth << "\n";
     for (unsigned long i = 0, j = 200000; i < 10; i++, j += 200000)
-        test_float_sort(j);
+        test_float_sort(j, fork_depth);
 
     return 0;
 }
--- exercises/templates/dac-int.cc
+++ exercises/templates/dac-int.cc
@@ -1,6 +1,7 @@
 #include "dac.h"
 
-void test_int_sort(const size_t size) {
+void test_int_sort(const size_t size,
+                   const unsigned int fork_depth) {
     int nums[size];
 
     for (size_t i = 0; i < size; i++)
@@ -12,16 +13,25 @@ void test_int_sort(const size_t size) {
     a->length = sizeof(nums) / sizeof(nums[0]);
 
     Timer t;
-    DC<int> sort(a);
+    DC<int> sort(a, 2, fork_depth);
     printf("Time to sort %7lu integers: %4ld ms\n", size, t.ms());
     sort.get()->isSorted();
 }
 
 int main(int argc, char *argv[]) {
+    unsigned int fork_depth = 1;
 
-    std::cout << "DC<int>\n";
+    if (argc == 2) {
+        unsigned int i = atoi(argv[1]);
+        if (i)
+            fork_depth = i;
+        else
+            std::cout << "warning: <fork_depth> must be an integer. Ignoring argument\n";
+    }
+
+    std::cout << "DC<int>, fork_depth = " << fork_depth << "\n";
     for (unsigned long i = 0, j = 200000; i < 10; i++, j += 200000)
-        test_int_sort(j);
+        test_int_sort(j, fork_depth);
 
     return 0;
 }
