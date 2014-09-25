a91a47b36b8caf2b45fc7b6c1523d0faa46c67bc
Chris Cummins <chrisc.101@gmail.com>
2014-09-25 13:02:44 +0100

    dac/benchmark: Reduce cpu_mult to 1.5

--- exercises/templates/dac/benchmark.sh
+++ exercises/templates/dac/benchmark.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-cpu_mult=2
+cpu_mult=1.5
 
 echo -n "Determining number of processing units (n)... "
 no_of_processors=$(nproc)
 echo "$no_of_processors"
 
 echo -n "Calculating maximum depth of parallelisation ($cpu_mult*n)... "
-test_limit=$((no_of_processors*cpu_mult))
+test_limit=$(echo "$cpu_mult*$no_of_processors" | bc -l | xargs printf '%.0f')
 echo "$test_limit"
 
 echo
@@ -24,7 +24,7 @@ cat $stable_sort.log >> .tmp.log0
 for (( i=0; i <= $test_limit; i++ )); do
     test=merge-sort-int
 
-    echo "Executing $test, with a parallelisation depth of $i..."
+    echo "Executing $test [$i of $test_limit]..."
     ./$test $i | tail -n+2 | awk '{print 4, $6};' > $test.log
 
     echo "d$i" > .tmp.log1
