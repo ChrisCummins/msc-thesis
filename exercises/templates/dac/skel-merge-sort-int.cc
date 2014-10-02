aa99f03132f2fd7d683cd1c8d974d5a87f01e3c6
Chris Cummins <chrisc.101@gmail.com>
2014-10-02 12:21:27 +0100

    dac: Minor refactors

--- exercises/templates/dac/skel-merge-sort-int.cc
+++ exercises/templates/dac/skel-merge-sort-int.cc
@@ -1,13 +1,14 @@
-#include "test.h"
+#include "./test.h"  // NOLINT(legal/copyright)
 
 int main(int argc, char *argv[]) {
+  std::cout << "skel::merge_sort<int>, "
+            << "parallelisation depth = "
+            << DAC_SKEL_PARALLELISATION_DEPTH << ", "
+            << "split threshold = "
+            << SKEL_MERGE_SORT_SPLIT_THRESHOLD << "\n";
 
-    std::cout << "skel::merge_sort<int>, "
-              << "parallelisation depth = " << DAC_SKEL_PARALLELISATION_DEPTH << ", "
-              << "split threshold = " << SKEL_MERGE_SORT_SPLIT_THRESHOLD << "\n";
+  for (int i = 0, j = 50000; i < 40; i++, j += 50000)
+    test_sort_func<int>(get_unsorted_int_vector(j), skel::merge_sort);
 
-    for (unsigned long i = 0, j = 50000; i < 40; i++, j += 50000)
-        test_sort_func<int>(get_unsorted_int_vector(j), skel::merge_sort);
-
-    return 0;
+  return 0;
 }
