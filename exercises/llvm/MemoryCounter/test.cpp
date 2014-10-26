65dd7c40fe076edaff7c021163e63ecb3e5c00c3
Chris Cummins <chrisc.101@gmail.com>
2014-10-26 16:13:57 +0000

    MemoryCounter/test: Better comments

--- exercises/llvm/MemoryCounter/test.cpp
+++ exercises/llvm/MemoryCounter/test.cpp
@@ -9,13 +9,19 @@ int g = 0;
 // The test function.
 //
 int foo() {
-    // Perform 10 STORE operations.
+    // BLOCK A: Perform 10 STORE operations.
     g = 1; g = 2; g = 3; g = 4; g = 5;
     g = 6; g = 7; g = 7; g = 9; g = 10;
 
+    /*
+     * Uncommenting this line will reduce the number of store
+     * operations by < 10, since we are not executing BLOCK B. If the
+     * number of store operations is reduced by 20, then we are losing
+     * the instrumentation for BLOCK A.
+     */
     // throw 0;
 
-    // Perform another 10 STORE operations.
+    // BLOCK B: Perform another 10 STORE operations.
     g = 11; g = 12; g = 13; g = 14; g = 15;
     g = 16; g = 17; g = 17; g = 19; g = 20;
 
