dd709865e660efb484fe5ad182cea0fe75a1d942
Chris Cummins <chrisc.101@gmail.com>
2014-11-05 18:35:57 +0000

    Add an experiments directory and first exp

---
+++ experiments/20141105-skel-opt-space/experiment.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2.7
+import os
+from datetime import datetime
+
+target = "test-skel-merge-sort-int"
+target_dir = "../../exercises/templates/dac/"
+split_threshold = [10, 1000, 100]
+max_recursion_depth = [0, 10, 1]
+test_size = [3, 6, 1]
+
+variable_a = split_threshold
+variable_b = max_recursion_depth
+variable_c = test_size
+
+num_of_runs = 10
+
+def prepare_experiment(split_threshold, parallelisation_depth, test_size):
+    os.system(("sed -ri 's/("
+               "DAC_SKEL_PARALLELISATION_DEPTH"
+               "\\s+)[0-9]+/\\1{0}/' {1}skel.h").format(parallelisation_depth, target_dir))
+    
+    os.system(("sed -ri 's/("
+               "SKEL_MERGE_SORT_SPLIT_THRESHOLD"
+               "\\s+)[0-9]+/\\1{0}/' {1}skel-merge-sort.h").format(split_threshold, target_dir))
+
+    os.system(("sed -ri 's/("
+               "TEST_SIZE"
+               "\\s+1e)[0-9]+/\\1{0}/' {1}{2}.cc").format(test_size, target_dir, target))
+    
+    os.system("make -C {0} {1} >/dev/null".format(target_dir, target))
+
+def run_experiment(a, b, c):
+    def get_run_time():
+        start = datetime.now()
+        os.system("{0}{1} >/dev/null".format(target_dir, target))
+        end = datetime.now()
+        return end - start
+
+    results = []
+    for i in range(num_of_runs):
+        results.append(get_run_time().microseconds)
+
+    print a, b, 10 ** c, sum(results) / float(len(results))
+
+print "split_threshold", "max_recursion_depth", "n", "time"
+for c in range(variable_c[0], variable_c[1] + variable_c[2], variable_c[2]):
+    for a in range(variable_a[0], variable_a[1] + variable_a[2], variable_a[2]):
+        for b in range(variable_b[0], variable_b[1] + variable_b[2], variable_b[2]):
+            prepare_experiment(a, b, c)
+            run_experiment(a, b, c)
---
+++ experiments/20141105-skel-opt-space/graph-results.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from matplotlib import cm
+from matplotlib.figure import Figure
+from matplotlib.mlab import griddata
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.cm as cm
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: graph-results.py <dataset> <n>")
+    sys.exit(1)
+
+r = mlab.csv2rec(sys.argv[1], delimiter=' ')
+n = sys.argv[2]
+
+x = []
+for i in r:
+    x.append(i[0])
+X = np.array(x)
+y = []
+for i in r:
+    y.append(i[1])
+Y = np.array(y)
+z = []
+for i in r:
+    z.append(i[3])
+
+# Convert all times into speedups relative to the first result.
+scale = z[0]
+for i in range(len(z)):
+    z[i] = scale / z[i]
+
+Z = np.array(z)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_zlabel('Speedup')
+
+# Speedup is between 1 and 4. Note this causes slowdowns to "fall
+# through the floor".
+ax.set_zlim(1, 4)
+
+# vmin and vmax are the limits of the colour maps.
+surf = ax.plot_trisurf(X, Y, Z, cmap=cm.jet, vmin=1.0, vmax=3.5, linewidth=.2)
+
+plt.suptitle('Optimisation space for merge sort, n=1e{0}'.format(n), fontsize=16)
+plt.ylabel('Parallelisation depth')
+plt.xlabel('Split threshold')
+plt.savefig('plot.png')
+plt.show()
