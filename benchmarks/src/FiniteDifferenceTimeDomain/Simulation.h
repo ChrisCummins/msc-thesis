f2f61aca644170ef79833eb7d01c3d7c53282872
Chris Cummins <chrisc.101@gmail.com>
2015-02-23 13:02:18 +0000

    benchmarks/FDTD: Whitespace fixes
    Issue #43.

--- benchmarks/src/FiniteDifferenceTimeDomain/Simulation.h
+++ benchmarks/src/FiniteDifferenceTimeDomain/Simulation.h
@@ -5,12 +5,12 @@
 #include "SkelCL/Stencil.h"
 #include <vector>
 
-typedef struct {	
-			float x;	
-			float y;	
-			float z;	
-			float w;	
-} data_t;				
+typedef struct {
+  float x;
+  float y;
+  float z;
+  float w;
+} data_t;
 
 using namespace skelcl;
 
@@ -18,60 +18,59 @@ class Simulation
 {
 public:
 
-	Simulation(const size_t size);
+  Simulation(const size_t size);
 
-	virtual ~Simulation();
+  virtual ~Simulation();
 
-	void initialize();
+  void initialize();
 
-	void run();
+  void run();
 
 private:
 
-	bool readRandomMediaFile(const char*);
+  bool readRandomMediaFile(const char*);
 
-	struct Parameters
-	{
-		float c;
-		float pi;
-		float hbar;
-		float mu0;
-		float eps0;
-		float log_2;
-		float c_mu0;
-		int abs_cell_size;
-		int array_size;
-		float tau32;
-		float tau21;
-		float tau10;
-		float dx;
-		float dt;
-		float dx_r;
-		float dt_r;
-		float omega_a;
-		float omega_a2;
-		float T2;
-		float c3;						// c³
-		float Nges;
-		float sqrt_mu0_eps0;
-		float sqrt_eps0_mu0;
-		float grid_size;
-		float resolution;
-		float time_end;
-		float abs_size;
-		float Pr;
-		int time_steps;
-	} _p;
+  struct Parameters
+  {
+    float c;
+    float pi;
+    float hbar;
+    float mu0;
+    float eps0;
+    float log_2;
+    float c_mu0;
+    int abs_cell_size;
+    int array_size;
+    float tau32;
+    float tau21;
+    float tau10;
+    float dx;
+    float dt;
+    float dx_r;
+    float dt_r;
+    float omega_a;
+    float omega_a2;
+    float T2;
+    float c3;						// c³
+    float Nges;
+    float sqrt_mu0_eps0;
+    float sqrt_eps0_mu0;
+    float grid_size;
+    float resolution;
+    float time_end;
+    float abs_size;
+    float Pr;
+    int time_steps;
+  } _p;
 
-	Matrix<data_t> E; 
-	Matrix<data_t> H;  
-	Matrix<data_t> N;
+  Matrix<data_t> E;
+  Matrix<data_t> H;
+  Matrix<data_t> N;
 
-	std::vector<data_t> _particles;
+  std::vector<data_t> _particles;
 
-	Map<data_t(data_t)>* motionEquation;
-	Stencil<data_t(data_t)>* eFieldEquation;
-	Stencil<data_t(data_t)>* hFieldEquation;
+  Map<data_t(data_t)>* motionEquation;
+  Stencil<data_t(data_t)>* eFieldEquation;
+  Stencil<data_t(data_t)>* hFieldEquation;
 
 };
-
