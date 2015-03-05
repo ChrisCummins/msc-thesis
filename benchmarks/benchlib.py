3bb178400c3ca739637c21b44283cd5f57c77c9f
Chris Cummins <chrisc.101@gmail.com>
2015-03-05 11:55:41 +0000

    benchmarks: Implement results caching

--- benchmarks/benchlib.py
+++ benchmarks/benchlib.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
+from atexit import register
 from datetime import datetime
 from hashlib import sha1
 from itertools import product
-from json import dump,dumps,load
 from math import sqrt
 from os import chdir,getcwd,listdir,makedirs
 from os.path import abspath,dirname,exists
@@ -12,6 +12,8 @@ from re import sub
 from socket import gethostname
 from subprocess import call,check_output
 from sys import exit,stdout
+
+import json
 
 ##### LOCAL VARIABLES #####
 
@@ -40,21 +42,6 @@ def bindir(name):
 def bin(name):
     return path(bindir(name), name)
 
-# Return the path of the results file for "version".
-def resultsfile(version=skelcl_version()):
-    return path(RESULTSDIR, '{0}.json'.format(version))
-
-# Return the results in "file".
-def loadresults(file):
-    if exists(file):
-        return load(open(file))
-    else:
-        return {}
-
-# Store "results" in "file".
-def store(results, file):
-    dump(results, open(file, 'w'),
-         sort_keys=True, indent=2, separators=(',', ': '))
 
 # Change to directory "path".
 def cd(path):
@@ -171,6 +158,60 @@ BUILDLOG = path(CWD, 'make.log')
 RUNLOG = path(CWD, 'run.log')
 
 
+###### EXPERIMENTAL RESULTS #####
+
+# Experimental results are stored in-memory in a cache, indexed by the
+# version. Results are read and written using the load() and store()
+# functions, respectively. On a cache miss, the load() function looks
+# up the name of the results file for that version and, if found, adds
+# it to the cache. A store() marks that version as "cachedirty", and
+# increments the "cachewrites" counter by 1. When the value of
+# "cachewrites" reaches "cachewritethreshold", the cache is emptied
+# and any file in the "cachedirty" set is written.
+_cache = {}
+_cachedirty = set()
+_cachewrites = 0
+_cachewritethreshold = 5
+
+# Return the path of the results file for "version".
+def _versionfile(version=skelcl_version()):
+    return path(RESULTSDIR, '{0}.json'.format(version))
+
+# Dump results cache.
+def _dumpcache_():
+    global _cachedirty, _cachewrites
+    for version in _cachedirty:
+        file = _versionfile(version)
+        json.dump(_cache[version], open(file, 'w'),
+                  sort_keys=True, indent=2, separators=(',', ': '))
+        print("Wrote '{0}'...".format(file))
+    _cachewrites = 0
+    _cachedirt = set()
+    print("Results cache clean.")
+
+# Register and exit handler.
+register(_dumpcache_)
+
+# Return the results for "file".
+def load(version):
+    if version in _cache:
+        return _cache[version]
+    else:
+        file = _versionfile(version)
+        if exists(file):
+            _cache[version] = json.load(open(file))
+        else:
+            _cache[version] = {}
+        return load(version) # recurse
+
+# Store "results" for "version".
+def store(results, version):
+    global _cachewrites
+    _cachedirty.add(version)
+    _cachewrites += 1
+    if _cachewrites >= _cachewritethreshold:
+        _dumpcache_()
+
 ###### BENCHMARK FUNCTIONS #####
 
 # Build example program "prog". If "clean", then clean before
@@ -217,11 +258,21 @@ def time(prog, args=[]):
         if match:
             return int(match.group(1))
 
+# Lookup results for "prog" with "args" on "id" under "version".
+def results(prog, args, id=ID(), version=skelcl_version()):
+    R = load(version)
+    options = ' '.join(args)
+
+    if prog in R:
+        if options in R[prog]:
+            if id in R[prog][options]:
+                return R[prog][options][id]
+    return []
+
 # Record the runtime of "prog" using "args", under experiment
 # "version".
-def record(prog, args=[], version=skelcl_version()):
-    f = resultsfile(version=version)
-    R = loadresults(f)
+def record(prog, args=[], version=skelcl_version(), n=100):
+    R = load(version)
     options = ' '.join(args)
     id = ID()
 
@@ -234,8 +285,10 @@ def record(prog, args=[], version=skelcl_version()):
     if id not in R[prog][options]:
         R[prog][options][id] = []
 
-    R[prog][options][id].append(time(prog, args))
-    store(R, f)
+    print("N:", n, "actual:", len(results(prog, args, version=version)))
+    if len(results(prog, args, version=version)) <= n:
+        R[prog][options][id].append(time(prog, args))
+        store(R, version)
 
 # Return all permutations of "options" for "prog"
 def permutations(prog, options=[[]]):
@@ -249,4 +302,4 @@ def iterate(prog, options=[[]], n=30, version=skelcl_version()):
         for options in P:
             args = [item for sublist in [x.split() for x in options] for item in sublist]
             print(prog, *options)
-            record(prog, args, version=version)
+            record(prog, args, version=version, n=i)
