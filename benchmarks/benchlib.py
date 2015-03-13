452aaa3ba2c4c0030240499991ba47af4a9059f4
Chris Cummins <chrisc.101@gmail.com>
2015-03-13 13:16:25 +0000

    benchmarks: Plot speedups and conf-intervals

--- benchmarks/benchlib.py
+++ benchmarks/benchlib.py
@@ -13,6 +13,9 @@ from socket import gethostname
 from subprocess import call,check_output
 from sys import exit,stdout
 
+import scipy
+from scipy import stats
+
 import json
 
 ##### LOCAL VARIABLES #####
@@ -144,7 +147,7 @@ def checksum(file):
 # Return the mean value of a list of divisible numbers.
 def mean(num):
     if len(num):
-        return sum(num) / len(num)
+        return sum([float(x) for x in num]) / float(len(num))
     else:
         return 0
 
@@ -159,6 +162,22 @@ def variance(num):
 # Return the standard deviation of a list of divisible numbers.
 def stdev(num):
     return sqrt(variance(num))
+
+# Return the confidence interval of a list for a given confidence
+def confinterval(l, c=0.95, n=30):
+    if len(l) > 1:
+        scale = stdev(l) / sqrt(len(l))
+
+        if len(l) >= n:
+            # For large values of n, use a normal (Gaussian) distribution:
+            c1, c2 = scipy.stats.norm.interval(c, loc=mean(l), scale=scale)
+        else:
+            # For small values of n, use a t-distribution:
+            c1, c2 = scipy.stats.t.interval(c, len(l) - 1, loc=mean(l), scale=scale)
+
+        return c1, c2
+    else:
+        return 0, 0
 
 
 ####### CONSTANTS & CONFIG #######
