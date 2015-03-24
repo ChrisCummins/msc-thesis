814add8a4ffc8b43405198551d000c6c06094fe5
Chris Cummins <chrisc.101@gmail.com>
2015-03-24 12:19:39 +0000

    benchmarks: Implement JSONCache

---
+++ benchmarks/jsoncache.py
@@ -0,0 +1,65 @@
+# jsoncache.py - Persistent store for JSON objects.
+#
+# There are two public methods: load() and store(). After loading from
+# disk, JSON objects are cached locally, and written to disk every
+# _CACHEWRITE_THRESHOLD writes, and when the process exits.
+from atexit import register
+
+import json
+
+_CACHEWRITE_THRESHOLD = 5
+
+_cachewrites = 0
+_cachedirty = set()
+_cache = {}
+
+def _readjson(path):
+    try:
+        data = json.load(open(path))
+        print("Read '{path}'...".format(path=path))
+        return data
+    except:
+        return {}
+
+def _writejson(path, data):
+    json.dump(data, open(path, 'w'),
+              sort_keys=True, indent=2, separators=(',', ': '))
+    print("Wrote '{path}'...".format(path=path))
+
+def _loadcache(path):
+    data = _readjson(path)
+    _cache[path] = data
+    return data
+
+def _writedirty():
+    global _cachedirty, _cachewrites
+
+    for path in _cachedirty:
+        _writejson(path, _cache[path])
+
+    # Reset cache status.
+    _cachewrites = 0
+    _cachedirty = set()
+
+def _dumpcache():
+    # There may be nothing to dump
+    if not _cachewrites:
+        return
+    _writedirty()
+
+# Register exit handler.
+register(_dumpcache)
+
+def _flagdirty(path):
+    global _cachedirty, _cachewrites
+
+    _cachedirty.add(path)
+    _cachewrites += 1
+    if _cachewrites >= _CACHEWRITE_THRESHOLD:
+        _dumpcache()
+
+def load(path):
+    return _cache[path] if path in _cache else _loadcache(path)
+
+def store(path):
+    _flagdirty(path)
