82f1a920e1ef69cbc766c81fb63198e49e7a3c3f
Chris Cummins <chrisc.101@gmail.com>
2015-03-30 16:02:50 +0100

    resultscache: Add plot path function

--- benchmarks/resultscache.py
+++ benchmarks/resultscache.py
@@ -40,31 +40,42 @@ class _HashableInvars:
     def __repr__(x):
         return str(x.__key()).encode('utf-8')
 
-def _path(benchmarkname, key, hostname):
-    return ("{root}/{benchmark}/{host}/{key}.json"
-            .format(root=config.RESULTS,
+def _path(benchmarkname, key, hostname, root, suffix, extension):
+    return ("{root}/{benchmark}/{host}/{key}{suffix}{extension}"
+            .format(root=root,
                     benchmark=benchmarkname,
-                    host=hostname, key=key))
+                    host=hostname, key=key,
+                    suffix=suffix, extension=extension))
 
 #
 def _loadresults(path):
     return jsoncache.load(path)
 
 #
-def _resultpath(invars):
+def _invars2path(invars, root=config.RESULTS, suffix="", extension=""):
     benchmark = lookup1(invars, BenchmarkName).val
     host = lookup1(invars, Hostname).val
     key = _HashableInvars(invars).key()
-    return _path(benchmark, key, host)
+    return _path(benchmark, key, host, root, suffix, extension)
+
+#
+def resultspath(invars, suffix="", extension=".json"):
+    return _invars2path(invars, root=config.RESULTS,
+                        suffix=suffix, extension=extension)
+
+#
+def plotpath(invars, suffix="", extension=".png"):
+    return _invars2path(invars, root=config.PLOTS,
+                        suffix=suffix, extension=extension)
 
 #
 def load(invars):
-    path = _resultpath(invars)
+    path = resultspath(invars)
     data = jsoncache.load(path)
 
     return Result.decode(data, invars)
 
 #
 def store(result):
-    path = _resultpath(result.invars)
+    path = resultspath(result.invars)
     jsoncache.store(path, result.encode())
