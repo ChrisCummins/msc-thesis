c2b2a6cba56d173ab5205d786c0ab12e14847b4f
Chris Cummins <chrisc.101@gmail.com>
2015-03-31 17:53:09 +0100

    gitfs: cd() to working directory before push

--- benchmarks/gitfs.py
+++ benchmarks/gitfs.py
@@ -38,6 +38,7 @@ def _commitandpush():
         cd(dirname(file))
         call(["git", "add", basename(file)])
 
+    cd(CWD)
     call(["git", "commit", "-m", "Auto-bot commit"])
     call(["git", "pull", "--rebase"])
     [call(["git", "push", remote, _REMOTES[remote]]) for remote in _REMOTES]
