61a9507df244f8b696ede8ecf659b1428239627f
Chris Cummins <chrisc.101@gmail.com>
2015-04-07 21:24:15 +0100

    benchmarks: Add an OpenCL host parser

---
+++ benchmarks/createOpenCLHost.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python2.7
+#
+from __future__ import print_function
+from os.path import dirname
+from re import search,sub
+
+from util import *
+from skelcl import *
+
+import config
+import json
+
+cd(path(SKELCL_BUILD, 'tools'))
+system(['make', 'OpenCLInfo'])
+
+system(['./OpenCLInfo'], out=open(config.RUNLOG, 'w'))
+
+output = [l.rstrip() for l in open(config.RUNLOG).readlines()]
+
+platformid = -1
+deviceid = -1
+
+platforms = []
+
+def addprop(name, key):
+    match = search(name + " *: *(.+)$", l)
+    if match:
+        platforms[platformid][deviceid][key] = match.group(1)
+
+for l in output:
+    match = search("^Platform [0-9]+:$", l)
+    if match:
+        platformid += 1 # Increment platform counter.
+        deviceid = -1 # Reset device counter.
+        platforms.append([]) # Add a new platform.
+        continue
+
+    match = search("Device [0-9]+: (.+)$", l)
+    if match:
+        deviceid += 1 # Increment device counter.
+        platforms[platformid].append({
+            "name": match.group(1)
+        }) # Add a new device.
+        continue
+
+    addprop("Device Version", "version")
+    addprop("OpenCL C Version", "opencl_version")
+    addprop("Compute Units", "compute_units")
+    addprop("Clock Frequency", "clock_frequency")
+    addprop("Global Memory Size", "global_memory_size")
+    addprop("Local Memory Size", "local_memory_size")
+    addprop("Max Work Group Size", "max_work_group_size")
+    match = search("Max Work Item Sizes *: *(.+)$", l)
+    if match:
+        platforms[platformid][deviceid]["max_work_item_sizes"] = json.loads(match.group(1))
+
+
+pprint(platforms)
