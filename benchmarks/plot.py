6633ec2ababf17e222dc6fd8f4c26a19516d3b1c
Chris Cummins <chrisc.101@gmail.com>
2015-04-12 22:27:46 +0100

    plot: Add 3d speedup plots

--- benchmarks/plot.py
+++ benchmarks/plot.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
 from hashlib import sha1
-from textwrap import wrap
 from matplotlib import rc
+from mpl_toolkits.mplot3d import Axes3D
+from textwrap import wrap
+import matplotlib.cm as cm
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -221,6 +223,109 @@ def speedups(speedups, err=[], labels=[], xlabel="", ylabel="Speedup",
         plt.figtext(.02, .02, caption)
         position[1] = .24 # Add extra padding to bottom.
         position[3] = .65
+
+    # Set the graph bounds.
+    plt.gca().set_position(position)
+
+    # Finish up.
+    _finalize(path)
+
+# Plot multiple rows of speedups. Speedups are passed in row-wise as a
+# list of evenyl sized list.
+def speedups3d(speedups, xlabel="", xlabels=[], ylabel="",
+               ylabels=[], zlabel="Speedup", title="", caption="",
+               baseline=-1, ymajorlines=False, path=None):
+    width = 1
+
+    # Matplotlib's 3D bar plot API accepts six lists of points for:
+    X = [] # Starting X (horizontal) position
+    Y = [] # Starting Y (depth) position
+    Z = [] # Starting Z (height) position
+    dX = width # Bar width
+    dY = width # Bar depth
+    dZ = [] # Bar height
+
+    xmax = len(speedups[0])
+    ymax = len(speedups)
+    zmax = max([max(row) for row in speedups])
+
+    # Serialise the list-of-lists to a flat list.
+    i = 0
+    for row in speedups:
+        rowlen = len(row)
+        X += list(range(rowlen))
+        Y += [i] * rowlen
+        Z += [0] * rowlen
+        dZ += row
+        i += 1
+
+    # Arguments to plt.bar3d()
+    kwargs = {}
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # 3D bar plot:
+    #
+    ax.bar3d(X, Y, Z, dX, dY, dZ, **kwargs)
+
+    # 3D surface plot:
+    #
+    # vmin and vmax are the limits of the colour maps.
+    # vmin, vmax = min(dZ), max(dZ)
+    # ax.plot_trisurf(X, Y, dZ, cmap=cm.jet, vmin=vmin, vmax=vmax, linewidth=.2)
+
+    #greenindexes = [x for x,v in enumerate(dZ) if v > 1]
+
+    # Filter out the bars from a complex plot.
+    bars = filter(lambda x: isinstance(x, matplotlib.patches.Rectangle), ax.get_children())
+
+    # # Colour the default value blue, and the speedups > 1 green.
+    # if baseline >= 0:
+    #     bars[baseline].set_facecolor('b')
+
+    for child in ax.get_children():
+        print(child)
+
+    # Use LaTeX text rendering.
+    fontsize=16
+    rc('text', usetex=True)
+    rc('font', size=fontsize)
+
+    #  Vertical major gridlines.
+    if ymajorlines:
+        ax.yaxis.grid(b=True, which='major', color="#aaaaaa", linestyle='-')
+
+    # Axis text and limits.
+    ax.set_ylabel(ylabel)
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    if zlabel:
+        ax.set_zlabel(zlabel)
+
+    #plt.ylim(ymin=0) # Values are always positive.
+    #plt.xlim(xmin=0, xmax=X[-1] + 1) # Plot as much data as needed.
+    plt.title('\n'.join(wrap(title, 60)), fontsize=fontsize, weight="bold")
+
+    plt.axhline(y=1, color='k')
+
+    if xlabels:
+        plt.xticks([x + width / 2 for x in range(xmax)],
+                   xlabels, fontsize=8, rotation=90)
+    if ylabels:
+        plt.yticks([x + width / 2 for x in range(ymax)],
+                   ylabels, fontsize=8, rotation=90)
+
+    position = [.12, # Left padding
+                .14, # Bottom padding
+                .85, # Width
+                .75] # Height
+
+    if caption:
+        plt.figtext(.02, .02, caption)
+        position[1] = .24 # Add extra padding to bottom.
+        position[3] = .65
+
 
     # Set the graph bounds.
     plt.gca().set_position(position)
