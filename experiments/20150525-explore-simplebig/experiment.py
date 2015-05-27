aa0d854b64d69678bd68816cf564b32091442d3b
Chris Cummins <chrisc.101@gmail.com>
2015-05-27 06:38:01 +0100

    Use arguments to SimpleBig

--- experiments/20150525-explore-simplebig/experiment.py
+++ experiments/20150525-explore-simplebig/experiment.py
@@ -1,7 +1,10 @@
 import itertools
 
 import labm8
+from labm8 import io
 from labm8 import fs
+from labm8 import system
+
 
 ROOT = fs.path("~/src/msc-thesis")
 
@@ -21,21 +24,43 @@ DATABASES = [
 ]
 DATABASE_ORACLE = "omnitune.oracle.db"
 
-BORDERS = (
-    ( 1,  1,  1,  1),
-    ( 5,  5,  5,  5),
-    (10, 10, 10, 10),
-    (20, 20, 20, 20),
-    (30, 30, 30, 30),
-    ( 1, 10, 30, 30),
-    (20, 10, 20, 10)
-)
+## Arguments
 
 COMPLEXITIES = ([""], ["-c"])
-DATASIZES = (
-    ["-w", "512", "-h", "512"],
-    ["-w", "1024", "-h", "1024"],
-    ["-w", "2048", "-h", "2048"]
-)
 
-ARGS = list(itertools.product(COMPLEXITIES, DATASIZES))
+BORDERS = [
+    ["--north",  "1", "--south",  "1", "--east",  "1", "--west",  "1"],
+    ["--north",  "5", "--south",  "5", "--east",  "5", "--west",  "5"],
+    ["--north", "10", "--south", "10", "--east", "10", "--west", "10"],
+    ["--north", "20", "--south", "20", "--east", "20", "--west", "20"],
+    ["--north", "30", "--south", "30", "--east", "30", "--west", "30"],
+    ["--north",  "1", "--south", "10", "--east", "30", "--west", "30"],
+    ["--north", "20", "--south", "10", "--east", "20", "--west", "10"]
+]
+
+DATASIZES = [
+    ["--width",  "512", "--height",  "512"],
+    ["--width", "1024", "--height", "1024"],
+    ["--width", "2048", "--height", "2048"]
+]
+
+if system.HOSTNAME == "cec":
+    DEVARGS = [["--device-type", "CPU", "--device-count", "1"]]
+elif system.HOSTNAME == "florence":
+    DEVARGS = [["--device-type", "CPU", "--device-count", "1"]]
+elif system.HOSTNAME == "monza":
+    DEVARGS = [["--device-type", "CPU", "--device-count", "1"],
+               ["--device-type", "GPU", "--device-count", "1"],
+               ["--device-type", "GPU", "--device-count", "2"]]
+elif system.HOSTNAME == "tim":
+    DEVARGS = [["--device-type", "CPU", "--device-count", "1"],
+               ["--device-type", "GPU", "--device-count", "1"],
+               ["--device-type", "GPU", "--device-count", "2"],
+               ["--device-type", "GPU", "--device-count", "3"],
+               ["--device-type", "GPU", "--device-count", "4"]]
+elif system.HOSTNAME == "whz5":
+    DEVARGS = [["--device-type", "GPU", "--device-count", "1"]]
+else:
+    io.fatal("Unrecognised hostname!")
+
+ARGS = list(itertools.product(COMPLEXITIES, DATASIZES, BORDERS, DEVARGS))
