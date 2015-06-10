8a0a0a517e007351ae088dcfe4b94ea08db055a8
Chris Cummins <chrisc.101@gmail.com>
2015-06-10 23:52:27 +0100

    Moar visualizzations

--- experiments/205-06-04-train/visualise.py
+++ experiments/205-06-04-train/visualise.py
@@ -4,7 +4,13 @@ from __future__ import division
 
 import sys
 
-import labm8
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from matplotlib.ticker import FormatStrFormatter
+
+import labm8 as lab
 from labm8 import io
 from labm8 import fs
 from labm8 import make
@@ -54,7 +60,7 @@ def create_max_wgsizes_heatmaps(db):
                   title="Distribution of maximum workgroup sizes")
 
 
-def eval_static_wgsizes(db):
+def create_coverage_reports(db):
     for i,device in enumerate(db.devices):
         io.debug("Device coverage", i, "...")
         where = ("scenario IN "
@@ -96,11 +102,36 @@ def eval_static_wgsizes(db):
                       .format(kernel), title=kernel)
 
 
+def create_params_plot(db):
+    summary = db.params_summary()
+    X = np.arange(len(summary))
+    Labels = [t[0] for t in summary]
+    Performance = [t[1] * 100 for t in summary]
+    Coverage = [t[2] * 100 for t in summary]
+    ax = plt.subplot(111)
+    ax.plot(X, Performance, 'b', label="Performance")
+    ax.plot(X, Coverage, 'g', label="Legality")
+    plt.gca().yaxis.set_major_formatter(FormatStrFormatter('%d%%'))
+    plt.xlim(xmin=0, xmax=len(X) - 1)
+    plt.ylim(ymin=0, ymax=100)
+    plt.tick_params(
+        axis='x',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelbottom='off') # labels along the bottom edge are off
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig("img/params.png")
+
+
 def main():
     db = _db.MLDatabase("~/data/msc-thesis/oracle.db")
 
-    eval_static_wgsizes(db)
+    create_params_plot(db)
+    lab.exit(0)
 
+    create_coverage_reports(db)
     create_oracle_wgsizes_heatmaps(db)
     create_max_wgsizes_heatmaps(db)
 
