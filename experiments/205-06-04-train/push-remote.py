4175aef10c7f1294a95c917d9e686a0985c1090a
Chris Cummins <chrisc.101@gmail.com>
2015-07-25 17:33:51 +0100

    push-remote: Load global configuration

--- experiments/205-06-04-train/push-remote.py
+++ experiments/205-06-04-train/push-remote.py
@@ -3,6 +3,8 @@
 from __future__ import division
 from __future__ import print_function
 
+import json
+
 import labm8 as lab
 from labm8 import fmt
 from labm8 import fs
@@ -21,18 +23,22 @@ from omnitune.skelcl.migrate import migrate
 import experiment
 
 
+def load_config(path="~/.omnitunerc.json"):
+    path = fs.abspath(path)
+    if fs.isfile(path):
+        return json.load(open(path))
+    else:
+        raise Exception("File '{}' not found!".format(path))
+
+
 def main():
     """
     Push local data to remote.
     """
-    MYSQL_CONFIG = {
-        "user": "chris",
-        "database": "omnitune",
-        "host": "dhcp-90-060"
-    }
 
+    cfg = load_config()
     db = migrate(_db.Database(experiment.ORACLE_PATH,
-                              remote=True, remote_cfg=MYSQL_CONFIG))
+                              remote=True, remote_cfg=cfg["remote"]))
     db.push_remote()
     db.status_report()
 
